8cf3b94be761e1483aee34e001ed41c1def0274a
Jeffrey Carandang <jeffreyvcarandang@gmail.com>
2019-06-19 20:29:55 +0800

    Update Text Color UI and use URLPopover

--- packages/format-library/src/text-color/index.js
+++ packages/format-library/src/text-color/index.js
@@ -1,21 +1,14 @@
 /**
- * External dependencies
- */
-import { get } from 'lodash';
-
-/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
-import { select } from '@wordpress/data';
-import { BlockControls } from '@wordpress/editor';
-import {
-	applyFormat,
-	removeFormat,
-	getActiveFormat,
-} from '@wordpress/rich-text';
-import { Toolbar, IconButton, Popover, ColorPalette } from '@wordpress/components';
+import { RichTextToolbarButton } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import InlineColorUI from './inline';
 
 const name = 'core/text-color';
 const title = __( 'Text Color' );
@@ -29,83 +22,44 @@ export const textColor = {
 		constructor() {
 			super( ...arguments );
 
+			this.onClose = this.onClose.bind( this );
+
 			this.state = {
 				isOpen: false,
 			};
+		}
+
+		onClose() {
+			this.setState( { isOpen: false } );
 		}
 
 		render() {
 			const {
 				value,
 				onChange,
+				isActive,
+				activeAttributes,
 			} = this.props;
-
-			let activeColor;
-
-			const colors = get( select( 'core/block-editor' ).getSettings(), [ 'colors' ], [] );
-			const activeColorFormat = getActiveFormat( value, name );
-
-			if ( activeColorFormat ) {
-				const styleColor = activeColorFormat.attributes.style;
-				if ( styleColor ) {
-					activeColor = styleColor.replace( new RegExp( `^color:\\s*` ), '' );
-				}
-			}
 
 			return (
 				<>
-					<BlockControls>
-						<Toolbar>
-							<IconButton
-								className="components-button components-icon-button components-inline-color-icon-button components-toolbar__control"
-								icon="editor-textcolor"
-								aria-haspopup="true"
-								tooltip={ title }
-								onClick={ () => this.setState( { isOpen: ! this.state.isOpen } ) }
-							>
-								<span
-									className="components-inline-color__indicator"
-									style={ {
-										backgroundColor: activeColor,
-									} }
-								/>
-							</IconButton>
-
-							{ this.state.isOpen && (
-								<Popover
-									position="bottom center"
-									className="components-inline-color-popover"
-									focusOnMount="container"
-									onClickOutside={ ( onClickOutside ) => {
-										if ( ( ! onClickOutside.target.classList.contains( 'components-dropdown-menu__toggle' ) && ! document.querySelector( '.components-dropdown-menu__toggle' ).contains( onClickOutside.target ) ) && ( ! document.querySelector( '.components-color-palette__picker' ) || ( document.querySelector( '.components-color-palette__picker' ) && ! document.querySelector( '.components-color-palette__picker' ).contains( onClickOutside.target ) ) ) ) {
-											this.setState( { isOpen: ! this.state.isOpen } );
-										}
-									} }
-								>
-									<ColorPalette
-										colors={ colors }
-										value={ activeColor }
-										onChange={ ( color ) => {
-											if ( color ) {
-												onChange(
-													applyFormat( value, {
-														type: name,
-														attributes: {
-															style: `color:${ color }`,
-														},
-													} )
-												);
-											} else {
-												onChange( removeFormat( value, name ) );
-											}
-										} }
-									>
-									</ColorPalette>
-								</Popover>
-							) }
-
-						</Toolbar>
-					</BlockControls>
+					<RichTextToolbarButton
+						icon="editor-textcolor"
+						title={ title }
+						onClick={ () => this.setState( { isOpen: true } ) }
+						isActive={ isActive }
+					/>
+					{ this.state.isOpen && (
+						<InlineColorUI
+							name={ name }
+							addingLink={ this.state.isOpen }
+							onClose={ this.onClose }
+							isActive={ isActive }
+							activeAttributes={ activeAttributes }
+							value={ value }
+							onChange={ onChange }
+						/>
+					) }
 				</>
 			);
 		}
---
+++ packages/format-library/src/text-color/inline.js
@@ -0,0 +1,136 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+import { Component, createRef, useMemo } from '@wordpress/element';
+import { select } from '@wordpress/data';
+import {
+	withSpokenMessages,
+	ColorPalette,
+} from '@wordpress/components';
+import { getRectangleFromRange } from '@wordpress/dom';
+import {
+	applyFormat,
+	removeFormat,
+	getActiveFormat,
+} from '@wordpress/rich-text';
+import { URLPopover } from '@wordpress/block-editor';
+
+const URLPopoverAtLink = ( { isActive, addingLink, value, ...props } ) => {
+	const anchorRect = useMemo( () => {
+		const selection = window.getSelection();
+		const range = selection.rangeCount > 0 ? selection.getRangeAt( 0 ) : null;
+		if ( ! range ) {
+			return;
+		}
+
+		if ( addingLink ) {
+			return getRectangleFromRange( range );
+		}
+
+		let element = range.startContainer;
+
+		// If the caret is right before the element, select the next element.
+		element = element.nextElementSibling || element;
+
+		while ( element.nodeType !== window.Node.ELEMENT_NODE ) {
+			element = element.parentNode;
+		}
+
+		const closest = element.closest( 'span' );
+		if ( closest ) {
+			return closest.getBoundingClientRect();
+		}
+	}, [ isActive, addingLink, value.start, value.end ] );
+
+	if ( ! anchorRect ) {
+		return null;
+	}
+
+	return <URLPopover anchorRect={ anchorRect } { ...props } />;
+};
+
+class InlineColorUI extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.onClickOutside = this.onClickOutside.bind( this );
+		this.resetState = this.resetState.bind( this );
+		this.autocompleteRef = createRef();
+
+		this.state = {
+			opensInNewWindow: false,
+			inputValue: '',
+		};
+	}
+
+	onClickOutside( event ) {
+		const colorpicker = document.querySelector( '.components-color-palette__picker' );
+		if ( ! colorpicker || ( colorpicker && ! colorpicker.contains( event.target ) ) ) {
+			this.resetState();
+		}
+	}
+
+	resetState() {
+		this.props.onClose();
+	}
+
+	render() {
+		const {
+			name,
+			value,
+			onChange,
+			isActive,
+			addingLink,
+		} = this.props;
+
+		let activeColor;
+
+		const colors = get( select( 'core/block-editor' ).getSettings(), [ 'colors' ], [] );
+		const activeColorFormat = getActiveFormat( value, name );
+
+		if ( activeColorFormat ) {
+			const styleColor = activeColorFormat.attributes.style;
+			if ( styleColor ) {
+				activeColor = styleColor.replace( new RegExp( `^color:\\s*` ), '' );
+			}
+		}
+
+		return (
+			<URLPopoverAtLink
+				value={ value }
+				isActive={ isActive }
+				addingLink={ addingLink }
+				onClickOutside={ this.onClickOutside }
+				onClose={ this.resetState }
+				className="components-inline-color-popover"
+			>
+				<ColorPalette
+					colors={ colors }
+					value={ activeColor }
+					onChange={ ( color ) => {
+						if ( color ) {
+							onChange(
+								applyFormat( value, {
+									type: name,
+									attributes: {
+										style: `color:${ color }`,
+									},
+								} )
+							);
+						} else {
+							onChange( removeFormat( value, name ) );
+						}
+					} }
+				>
+				</ColorPalette>
+			</URLPopoverAtLink>
+		);
+	}
+}
+
+export default withSpokenMessages( InlineColorUI );
