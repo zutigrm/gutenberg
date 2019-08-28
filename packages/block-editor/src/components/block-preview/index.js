37214aad2197c2a26339abbc57f8f3d0a2b89fc0
retrofox <rdsuarez@gmail.com>
2019-08-28 18:47:23 -0300

    block-preview: add onReady callback property

--- packages/block-editor/src/components/block-preview/index.js
+++ packages/block-editor/src/components/block-preview/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { castArray } from 'lodash';
+import { castArray, noop } from 'lodash';
 import classnames from 'classnames';
 
 /**
@@ -18,7 +18,7 @@ import BlockEditorProvider from '../provider';
 import BlockList from '../block-list';
 import { getBlockPreviewContainerDOMNode } from '../../utils/dom';
 
-function ScaledBlockPreview( { blocks, viewportWidth } ) {
+function ScaledBlockPreview( { blocks, viewportWidth, onReady } ) {
 	const previewRef = useRef( null );
 
 	const [ isReady, setIsReady ] = useState( false );
@@ -55,9 +55,17 @@ function ScaledBlockPreview( { blocks, viewportWidth } ) {
 
 				// Hack: we need  to reset the scaled elements margins
 				previewElement.style.marginTop = '0';
+
+				onReady( {
+					scale,
+					position: { x: offsetX * scale, y: offsetY },
+					ref: previewRef,
+				} );
 			} else {
 				const containerElementRect = containerElement.getBoundingClientRect();
-				setPreviewScale( containerElementRect.width / viewportWidth );
+				const scale = containerElementRect.width / viewportWidth;
+				setPreviewScale( scale );
+				onReady( { scale, ref: previewRef } );
 			}
 
 			setIsReady( true );
@@ -98,7 +106,7 @@ function ScaledBlockPreview( { blocks, viewportWidth } ) {
 	);
 }
 
-export function BlockPreview( { blocks, viewportWidth = 700, settings } ) {
+export function BlockPreview( { blocks, viewportWidth = 700, settings, onReady = noop } ) {
 	const renderedBlocks = useMemo( () => castArray( blocks ), [ blocks ] );
 	const [ recompute, triggerRecompute ] = useReducer( ( state ) => state + 1, 0 );
 	useLayoutEffect( triggerRecompute, [ blocks ] );
@@ -118,6 +126,7 @@ export function BlockPreview( { blocks, viewportWidth = 700, settings } ) {
 				key={ recompute }
 				blocks={ renderedBlocks }
 				viewportWidth={ viewportWidth }
+				onReady={ onReady }
 			/>
 		</BlockEditorProvider>
 	);
