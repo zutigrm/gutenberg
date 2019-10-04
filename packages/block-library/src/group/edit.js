bb4be23045506d2f2811d0e0ef4e06c9fe6e1062
Jorge <jorge.costa@developer.pt>
2019-10-04 19:24:17 +0100

    Add test code to group block

--- packages/block-library/src/group/edit.js
+++ packages/block-library/src/group/edit.js
@@ -2,19 +2,49 @@
  * External dependencies
  */
 import classnames from 'classnames';
+import { partialRight } from 'lodash';
 
 /**
  * WordPress dependencies
  */
+import { useState } from '@wordpress/element';
 import { withSelect } from '@wordpress/data';
 import { compose } from '@wordpress/compose';
 import { __ } from '@wordpress/i18n';
 import {
+	DimensionControl,
 	InspectorControls,
 	InnerBlocks,
 	PanelColorSettings,
 	withColors,
 } from '@wordpress/block-editor';
+
+function TestDimensionControl() {
+	const [ dimensions, setDimensions ] = useState( {} );
+	const { paddingSize } = dimensions;
+
+	const updateSpacing = ( dimension, size, device = '' ) => {
+		setDimensions( {
+			...dimensions,
+			[ `${ dimension }${ device }` ]: size,
+		} );
+	};
+	const resetSpacingDimension = ( dimension, device = '' ) => {
+		setDimensions( {
+			...dimensions,
+			[ `${ dimension }${ device }` ]: '',
+		} );
+	};
+	return (
+		<DimensionControl
+			label={ __( 'Padding' ) }
+			icon={ 'desktop' }
+			onReset={ partialRight( resetSpacingDimension, 'paddingSize' ) }
+			onSpacingChange={ partialRight( updateSpacing, 'paddingSize' ) }
+			currentSize={ paddingSize }
+		/>
+	);
+}
 
 function GroupEdit( {
 	className,
@@ -43,6 +73,7 @@ function GroupEdit( {
 						},
 					] }
 				/>
+				<TestDimensionControl />
 			</InspectorControls>
 			<div className={ classes } style={ styles }>
 				<div className="wp-block-group__inner-container">
