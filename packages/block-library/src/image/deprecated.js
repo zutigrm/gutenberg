72fb014b7c47c89885d0b496206067e0548d1ba1
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-04-30 17:10:14 +1000

    Add deprecation for moving border radius to inner image element
    New border block support styles and classes need to be on the inner img element. As a result it makes sense to move the border radius style with them. This requires a deprecation which this commit provides along with new fixtures.

--- packages/block-library/src/image/deprecated.js
+++ packages/block-library/src/image/deprecated.js
@@ -31,6 +31,12 @@ const blockAttributes = {
 		source: 'html',
 		selector: 'figcaption',
 	},
+	title: {
+		type: 'string',
+		source: 'attribute',
+		selector: 'img',
+		attribute: 'title',
+	},
 	href: {
 		type: 'string',
 		source: 'attribute',
@@ -57,6 +63,9 @@ const blockAttributes = {
 	},
 	height: {
 		type: 'number',
+	},
+	sizeSlug: {
+		type: 'string',
 	},
 	linkDestination: {
 		type: 'string',
@@ -289,6 +298,93 @@ const deprecated = [
 							value={ caption }
 						/>
 					) }
+				</figure>
+			);
+		},
+	},
+	{
+		attributes: blockAttributes,
+		supports: {
+			anchor: true,
+			color: {
+				__experimentalDuotone: 'img',
+				text: false,
+				background: false,
+				__experimentalBorder: {
+					radius: true,
+				},
+			},
+		},
+		save( { attributes } ) {
+			const {
+				url,
+				alt,
+				caption,
+				align,
+				href,
+				rel,
+				linkClass,
+				width,
+				height,
+				id,
+				linkTarget,
+				sizeSlug,
+				title,
+			} = attributes;
+
+			const newRel = isEmpty( rel ) ? undefined : rel;
+
+			const classes = classnames( {
+				[ `align${ align }` ]: align,
+				[ `size-${ sizeSlug }` ]: sizeSlug,
+				'is-resized': width || height,
+			} );
+
+			const image = (
+				<img
+					src={ url }
+					alt={ alt }
+					className={ id ? `wp-image-${ id }` : null }
+					width={ width }
+					height={ height }
+					title={ title }
+				/>
+			);
+
+			const figure = (
+				<>
+					{ href ? (
+						<a
+							className={ linkClass }
+							href={ href }
+							target={ linkTarget }
+							rel={ newRel }
+						>
+							{ image }
+						</a>
+					) : (
+						image
+					) }
+					{ ! RichText.isEmpty( caption ) && (
+						<RichText.Content
+							tagName="figcaption"
+							value={ caption }
+						/>
+					) }
+				</>
+			);
+
+			if ( 'left' === align || 'right' === align || 'center' === align ) {
+				return (
+					<div { ...useBlockProps.save() }>
+						<figure className={ classes }>{ figure }</figure>
+					</div>
+				);
+			}
+
+			return (
+				<figure { ...useBlockProps.save( { className: classes } ) }>
+					{ figure }
 				</figure>
 			);
 		},
