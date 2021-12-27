8084551ba9b8548f3b750c8a024646b1475dccf3
David Calhoun <438664+dcalhoun@users.noreply.github.com>
2021-12-27 08:47:13 -0600

    Add Flipper integration

--- packages/react-native-editor/ios/GutenbergDemo/AppDelegate.swift
+++ packages/react-native-editor/ios/GutenbergDemo/AppDelegate.swift
@@ -1,13 +1,20 @@
 import Foundation
 import UIKit
 
+#if DEBUG
+#if FB_SONARKIT_ENABLED
+import FlipperKit
+#endif
+#endif
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        initializeFlipper(with: application)
+
         AppDelegate.setupUITest()
         
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -30,4 +37,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     static func isUITesting() -> Bool {
         return ProcessInfo.processInfo.arguments.contains("uitesting")
     }
+
+    private func initializeFlipper(with application: UIApplication) {
+        #if DEBUG
+        #if FB_SONARKIT_ENABLED
+            let client = FlipperClient.shared()
+            let layoutDescriptorMapper = SKDescriptorMapper(defaults: ())
+            FlipperKitLayoutComponentKitSupport.setUpWith(layoutDescriptorMapper)
+            client?.add(FlipperKitLayoutPlugin(rootNode: application, with: layoutDescriptorMapper!))
+            client?.add(FKUserDefaultsPlugin(suiteName: nil))
+            client?.add(FlipperKitReactPlugin())
+            client?.add(FlipperKitNetworkPlugin(networkAdapter: SKIOSNetworkAdapter()))
+            client?.add(FlipperReactPerformancePlugin.sharedInstance())
+            client?.start()
+        #endif
+        #endif
+  }
 }
