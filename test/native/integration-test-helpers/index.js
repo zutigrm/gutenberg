b3135e4a5bc8fccfff054e0795454a9a77bb9dcf
Carlos Garcia <fluiddot@gmail.com>
2022-02-07 18:33:12 +0100

    Add addBlock helper

--- test/native/integration-test-helpers/index.js
+++ test/native/integration-test-helpers/index.js
@@ -1,3 +1,4 @@
+export { addBlock } from './add-block';
 export { executeStoreResolvers } from './execute-store-resolvers';
 export { getEditorHtml } from './get-editor-html';
 export { initializeEditor } from './initialize-editor';
