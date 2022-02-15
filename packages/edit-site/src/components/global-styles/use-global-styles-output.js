4e3c270db579393d541ee24e715d6334f8550893
Alex Lende <alex@lende.xyz>
2022-02-14 23:30:08 -0600

    Revert "Try rendering SVGs from server"
    This reverts commit a49344372a24af21b9a111a8346f37ae620a3533.

--- packages/edit-site/src/components/global-styles/use-global-styles-output.js
+++ packages/edit-site/src/components/global-styles/use-global-styles-output.js
@@ -22,6 +22,7 @@ import {
 	__EXPERIMENTAL_ELEMENTS as ELEMENTS,
 	getBlockTypes,
 } from '@wordpress/blocks';
+import { withFilters } from '@wordpress/components';
 import { useEffect, useState, useContext } from '@wordpress/element';
 import { getCSSRules } from '@wordpress/style-engine';
 
@@ -131,6 +132,27 @@ function getPresetsClasses( blockSelector, blockPresets = {} ) {
 			return declarations;
 		},
 		''
+	);
+}
+
+const PresetSvgFilter = withFilters( 'editor.PresetSvgFilter' )( () => null );
+
+function getPresetsSvgFilters( blockPresets = {} ) {
+	return PRESET_METADATA.filter( ( metadata ) => metadata.svgFilter ).flatMap(
+		( metadata ) => {
+			const presetByOrigin = get( blockPresets, metadata.path, {} );
+			return [ 'default', 'theme' ]
+				.filter( ( origin ) => presetByOrigin[ origin ] )
+				.flatMap( ( origin ) =>
+					presetByOrigin[ origin ].map( ( preset ) => (
+						<PresetSvgFilter
+							metadata={ metadata }
+							preset={ preset }
+							key={ preset.slug }
+						/>
+					) )
+				);
+		}
 	);
 }
 
@@ -371,6 +393,13 @@ export const toStyles = ( tree, blockSelectors ) => {
 	return ruleset;
 };
 
+export function toSvgFilters( tree, blockSelectors ) {
+	const nodesWithSettings = getNodesWithSettings( tree, blockSelectors );
+	return nodesWithSettings.flatMap( ( { presets } ) => {
+		return getPresetsSvgFilters( presets );
+	} );
+}
+
 const getBlockSelectors = ( blockTypes ) => {
 	const result = {};
 	blockTypes.forEach( ( blockType ) => {
@@ -390,6 +419,7 @@ const getBlockSelectors = ( blockTypes ) => {
 export function useGlobalStylesOutput() {
 	const [ stylesheets, setStylesheets ] = useState( [] );
 	const [ settings, setSettings ] = useState( {} );
+	const [ svgFilters, setSvgFilters ] = useState( {} );
 	const { merged: mergedConfig } = useContext( GlobalStylesContext );
 
 	useEffect( () => {
@@ -403,6 +433,7 @@ export function useGlobalStylesOutput() {
 			blockSelectors
 		);
 		const globalStyles = toStyles( mergedConfig, blockSelectors );
+		const filters = toSvgFilters( mergedConfig, blockSelectors );
 		setStylesheets( [
 			{
 				css: customProperties,
@@ -414,7 +445,8 @@ export function useGlobalStylesOutput() {
 			},
 		] );
 		setSettings( mergedConfig.settings );
+		setSvgFilters( filters );
 	}, [ mergedConfig ] );
 
-	return [ stylesheets, settings ];
+	return [ stylesheets, settings, svgFilters ];
 }
