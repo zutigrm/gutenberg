6fbd3e4578c36a03e174b5d6e4822aa2ca0d4631
Daniel Richards <daniel.richards@automattic.com>
2022-03-23 13:35:30 +0800

    Expose defaultAllowedBlockTypes in block editor settings

--- packages/editor/src/components/provider/use-block-editor-settings.js
+++ packages/editor/src/components/provider/use-block-editor-settings.js
@@ -98,6 +98,7 @@ function useBlockEditorSettings( settings, hasTemplate ) {
 				'bodyPlaceholder',
 				'codeEditingEnabled',
 				'colors',
+				'defaultAllowedBlockTypes',
 				'disableCustomColors',
 				'disableCustomFontSizes',
 				'disableCustomGradients',
